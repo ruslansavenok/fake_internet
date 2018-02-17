979b9ca253a94dacd1a9dc9c882c4a6e08c97e79
Ruslan <ruslan.savenok@gmail.com>
2018-02-17 18:44:05 +0100

    test

--- lib/fake_internet_web/endpoint.ex
+++ lib/fake_internet_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule FakeInternetWeb.Endpoint do
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   plug Plug.Static,
-    at: "/uploads", from: Path.expand("./uploads"), gzip: false
+    at: "/uploads", from: "./uploads", gzip: false
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
