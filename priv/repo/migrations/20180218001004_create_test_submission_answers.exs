7d9f65690abf09a00a41424944fe93cd0a2075c2
Ruslan <ruslan.savenok@gmail.com>
2018-02-18 14:13:42 +0100

    Fix migration

--- priv/repo/migrations/20180218001004_create_test_submission_answers.exs
+++ priv/repo/migrations/20180218001004_create_test_submission_answers.exs
@@ -4,7 +4,7 @@ defmodule FakeInternet.Repo.Migrations.CreateTestSubmissionAnswers do
   def change do
     create table(:test_submission_answers) do
       add :test_submission_id, references(:test_submissions, on_delete: :nothing)
-      add :question_id, references(:questions, on_delete: nothing)
+      add :question_id, references(:questions, on_delete: :nothing)
       add :answer_id, references(:question_answers, on_delete: :nothing)
 
       timestamps()
